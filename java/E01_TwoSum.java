3459f865b9ed6c7e5660c86f2127cc7880508fa3
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-06-13 23:38:22 -0300

    E01 - Update - Java

--- java/E01_TwoSum.java
+++ java/E01_TwoSum.java
@@ -30,23 +30,19 @@ import java.io.IOException;
 
 public class E01_TwoSum {
     public static void main(String[] args) throws IOException{
-        int[] nums = {2,7,11,15};
-        int target = 9;
+        int[] nums = new int[]{};   //value reported for the exercise
+        int target = 0;             //value reported for the exercise
         twoSum(nums, target);
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        int [] result = new int[2];
-
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length; j++) {
                 if(nums[i] + nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
+                    return new int[]{i, j};
                 }
             }
         }
-
-        return result;
+        return new int[]{};
     }
 }
